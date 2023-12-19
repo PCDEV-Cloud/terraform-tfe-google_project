8a77c3efa406c87d5c15ea06e8abb9b78bf7548f
Paweł Cholewa <pawel.cholewa@tsh.io>
2023-12-19 17:19:23 +0100

    Delete name validation

--- variables.tf
+++ variables.tf
@@ -1,21 +1,6 @@
 variable "name" {
   type        = string
   description = ""
-
-  validation {
-    condition     = can(regex("^[a-zA-Z][a-zA-Z0-9 _-]{1,28}[a-zA-Z0-9]$", var.name))
-    error_message = "The name must be 3 to 30 characters in length. Can only contain letters, numbers, inner spaces hyphens (-) and underscores (_). Must start with a letter and end with a letter or number."
-  }
-
-  validation {
-    condition     = !can(regex("^.*--.*$", var.name))
-    error_message = "Hyphens cannot appear next to each other."
-  }
-
-  validation {
-    condition     = !can(regex("^.*__.*$", var.name))
-    error_message = "Underscores cannot appear next to each other."
-  }
 }
 
 variable "randomize_name" {
